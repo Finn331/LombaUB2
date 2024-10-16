14ab51be50385a3e1c99bf376ef0f808d9d636ed
Carlo <93345815+Finn331@users.noreply.github.com>
2024-10-16 14:45:42 +0700

    Dialogue System
    disini gw menambahkan Dialogue System

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,41 +7,61 @@ using UnityEngine.UI;
 public class PlayerStatus : MonoBehaviour
 {
     [Header("Health Setting")]
-    [SerializeField] int maxHealth;
-    public int currentHealth;
-    [SerializeField] Slider healthSlider;
-    [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] int maxHealth;  // Maksimal kesehatan
+    public int currentHealth;              // Kesehatan saat ini
+    [SerializeField] Slider healthSlider;  // Slider untuk menampilkan kesehatan
+    /*[SerializeField] TextMeshProUGUI healthText;*/  // Text untuk menampilkan kesehatan
 
     // Start is called before the first frame update
     void Start()
     {
+        // Inisialisasi currentHealth dengan maxHealth pada awal permainan
+        currentHealth = maxHealth;
+
+        // Inisialisasi Slider dan Text
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdateHealthUI();
     }
 
-    //public void TakeDamage(int damage)
-    //{
-    //    currentHealth -= damage;
-    //    healthSlider.value = currentHealth;
-    //    if (currentHealth <= 0)
-    //    {
-    //        Die();
-    //    }
-    //}
-
-    void HealthSlider()
+    // Method untuk menerima damage
+    public void TakeDamage(int damage)
     {
-        healthSlider.maxValue = maxHealth;
-        healthSlider.value = currentHealth;
+        currentHealth -= damage;
+
+        // Pastikan currentHealth tidak lebih kecil dari 0
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        // Update UI setelah menerima damage
+        UpdateHealthUI();
+
+        // Jika currentHealth <= 0, panggil fungsi Die
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
 
-    void HealthText()
+    // Method untuk memperbarui slider dan teks kesehatan
+    void UpdateHealthUI()
     {
-        healthText.text = currentHealth.ToString() + "/" + maxHealth.ToString();
+        healthSlider.maxValue = maxHealth;  // Set nilai maksimal slider
+        healthSlider.value = currentHealth; // Set nilai slider berdasarkan kesehatan saat ini
+
+        /*healthText.text = currentHealth.ToString() + "/" + maxHealth.ToString();*/ // Perbarui teks kesehatan
+    }
+
+    // Method ketika player mati
+    void Die()
+    {
+        // Logika untuk saat player mati (misalnya, restart level, game over, dll.)
+        Debug.Log("Player has died.");
     }
 }
